38f94a880e844f62833153ed65ed348b913d4404
silvio <silvio@Silvio>
2018-05-30 18:23:37 +0200

    [Silvio] Refactor lieve

--- src/main/java/it/uniroma3/spring/model/CategoriaAttività.java
+++ src/main/java/it/uniroma3/spring/model/CategoriaAttività.java
@@ -1,12 +1,9 @@
 package it.uniroma3.spring.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class CategoriaAttività
